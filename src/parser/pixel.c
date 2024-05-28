d806c3662a8cc4499e8d8d41e713ccea7a5254ec
Luiz-Pastor <luigpacr5@gmail.com>
2024-05-28 09:21:53 +0200

    Added utils to manage the pixels

--- src/parser/pixel.c
+++ src/parser/pixel.c
@@ -15,3 +15,22 @@ int	pixel_is_valid(t_pixel *pixel)
 		pixel->blue < PIXEL_MIN || pixel->blue > PIXEL_MAX
 	);
 }
+
+int	pixel_is_default(t_pixel *pixel)
+{
+	return (
+		pixel->red == PIXEL_DEFAULT &&
+		pixel->green == PIXEL_DEFAULT &&
+		pixel->blue == PIXEL_DEFAULT
+	);
+}
+
+void	pixel_copy(t_pixel *dest, t_pixel *src)
+{
+	if (!dest || !src)
+		return ;
+	
+	dest->red = src->red;
+	dest->green = src->green;
+	dest->blue = src->blue;
+}
