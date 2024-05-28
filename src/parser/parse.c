831d7dfcc675453de4a3eb3f9fb0759b275e868b
Luiz-Pastor <luigpacr5@gmail.com>
2024-05-28 18:10:49 +0200

    Added extension checker and the call to the function to save the config data

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -1,25 +1,36 @@
 #include "../../include/cub3d.h"
 
-static void	parse_errors(int error)
+static void	parse_error(int error, t_parsed_data *parsed)
 {
 	printf("[ Error ] ");
 	if (error == ERROR_INVALID_ARGS)
 		printf("Invalid number of arguments!\n");
+	else if (error == ERROR_OPEN)
+		printf("Error opening the config file\n");
 	else
 		printf("Success(\\\\(·o·;)\n");
+
+	if (parsed)
+		parse_print(parsed);
+
+
 	exit(1);
 }
 
-static void	init_parser_data(t_parsed_data *parsed)
+static void	init_parser_data(t_parsed_data *parsed, char *filename)
 {
 	size_t	index;
 	
+	/* Clean the struct data */
+	ft_bzero(parsed, sizeof(*parsed));
+
 	/* Init the main fd */
-	parsed->fd = -1;
+	parsed->fd = open(filename, O_RDONLY);
+	if (parsed->fd < 0)
+		parse_error(ERROR_OPEN, NULL);
 
 	/* Init the strings and the fds */
 	index = -1;
-	ft_bzero(parsed, sizeof(*parsed));
 	while (++index < TEXTURES_COUNT)
 		parsed->textures_fds[index] = -1;
 	
@@ -28,22 +39,21 @@ static void	init_parser_data(t_parsed_data *parsed)
 	reset_pixel(&parsed->ceiling);
 }
 
+// static int	valid_exten
+
 /*#########################################################*/
 
 void	parse(int argc, char **argv, t_parsed_data *parsed)
 {
-	(void) argv, (void)parsed;
-	if (argc != 2)
-		parse_errors(ERROR_INVALID_ARGS);
+	
+	if (argc != 2 || ft_strcmp(CONFIG_EXTENSION, &argv[1][ft_strlen(argv[1]) - 4]))
+		parse_error(ERROR_INVALID_ARGS, NULL);
 
-	init_parser_data(parsed);
-	/* TODO: Rest of parse */
-}
-
-void	parse_print(int output, t_parsed_data *parsed)
-{
-	(void)output, (void)parsed;
-	/* TODO: msg to report the parser */
+	init_parser_data(parsed, argv[1]);
+	//parse_file(parsed);
+	
+	parse_print(parsed);
+	exit(0);
 }
 
 void	destroy_parsed(t_parsed_data *parsed)
