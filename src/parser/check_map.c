d109b6046a38fdb1438e07a9929bb030bac453a3
Luiz-Pastor <luigpacr5@gmail.com>
2024-06-03 09:39:23 +0200

    Changed returns

--- src/parser/check_map.c
+++ src/parser/check_map.c
@@ -1,6 +1,6 @@
 #include "../../include/cub3d.h"
 
-static int	check_characters(char **map)
+static int	check_characters(char **map, char *set)
 {
 	size_t	i;
 	size_t	j;
@@ -11,20 +11,20 @@ static int	check_characters(char **map)
 		j = 0;
 		while (map[i][++j])
 		{
-			if (!ft_strchr(VALID_MAP_CHARS, map[i][j]))
-				return 0;
+			if (!ft_strchr(set, map[i][j]))
+				return 1;
 		}
 	}
-	return 1;
+	return 0;
 }
 
 
 void	check_map(t_parsed_data *parsed)
 {
 	/* Check that aren't strange characters */
-	check_characters(parsed->map);
+	check_characters(parsed->map, VALID_MAP_CHARS);
 
-	/* Check the flood fill */
+	/* Check the flood fill: 1 in case if errro; 0 otherwise */
 	flood_fill(parsed->map);
 
 	/* Check door texture if there is any door */
