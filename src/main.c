a40f7902561bd17405149bfaf218309b27e9c314
Angela Barrio Magdalena <abarrio-@c1r11s2.42madrid.com>
2024-06-04 11:02:33 +0200

    delete func useles

--- src/main.c
+++ src/main.c
@@ -1,22 +1,5 @@
 #include "../include/cub3d.h"
 
-int error(int msg)
-{
-    if (msg == ERROR_INVALID_ARGS)
-        ft_printf("%sINVALID ARGS, INSERT A MAP: .cub\n%s", C_ORANGE, CLEAR);
-    else if (msg == ERROR_OPEN)
-        ft_printf("%sOPEN ERROR\n%s", C_ORANGE, CLEAR);
-    else if (msg == ERROR_MALLOC)
-        ft_printf("%sMALLOC ERROR\n%s", C_ORANGE, CLEAR);
-    else if (msg == ERROR_FILE_CONTENT)
-        ft_printf("%sINVALID FILE CONTENT\n%s", C_ORANGE, CLEAR);
-    else if (msg == ERROR_MLX_TEX_CREATE)
-        ft_printf("%sTEXTURE CREATE FAIL MLX FUNCTION\n%s", C_ORANGE, CLEAR);
-    else if (msg == ERROR_MLX_INIT)
-        ft_printf("%sINIT FAIL MLX FUNCTION\n%s", C_ORANGE, CLEAR);
-    return(1);
-}
-
 void    print_data(t_cube *cube)
 {
     printf("======== DATA ========\n");
