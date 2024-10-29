b3fefb422731ecbb80291f52f2fd4e30a886eaf4
angiebm4 <angelabarrio2000@gmail.com>
2024-10-29 10:43:45 +0100

    delete useless things

--- src/cube_init/minimap_init.c
+++ src/cube_init/minimap_init.c
@@ -6,7 +6,7 @@
 /*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:04:27 by abarrio-          #+#    #+#             */
-/*   Updated: 2024/10/28 13:56:47 by abarrio-         ###   ########.fr       */
+/*   Updated: 2024/10/29 10:42:49 by abarrio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,15 @@ static void	create_image_map(t_cube *cube)
 	cube->graphic->mini_map->img = mlx_new_image(cube->graphic->mlx,
 			MINIMAP_WIDTH, MINIMAP_HEIGHT);
 	if (cube->graphic->mini_map->img == NULL)
-		exit(1); /* TODO: malloc error*/
-	cube->graphic->mini_map->data_addr = 
-			mlx_get_data_addr(cube->graphic->mini_map->img,
-			&cube->graphic->mini_map->bpp, 
-			&cube->graphic->mini_map->size_line,
+		exit(1);
+	cube->graphic->mini_map->data_addr = mlx_get_data_addr(
+			cube->graphic->mini_map->img,
+			&cube->graphic->mini_map->bpp, &cube->graphic->mini_map->size_line,
 			&cube->graphic->mini_map->endian);
 }
 
 void	minimap_init(t_cube *cube)
 {
-	/* TODO: minimap can be a t_image without pointer... */
 	cube->graphic->mini_map = ft_calloc(1, sizeof(t_image));
 	if (!cube->graphic->mini_map)
 		cube_destroy(cube, 1);
